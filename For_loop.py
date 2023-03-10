b0ca0864740227383bfdb754e4ff272233633ced
bappych <bappy4l@hotmai.com>
2023-03-10 15:42:35 +0000

    Added comment

--- For_loop.py
+++ For_loop.py
@@ -1,5 +1,6 @@
 number_of_names = 23
 
-for i in range (0, number_of_names):
-    print(i+1)
+for i in range(0, number_of_names):
+    print(i + 1)
 
+# This is a new line added to check if the Git is working or not
