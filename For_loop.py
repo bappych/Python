f5faab7bb122ef04bf59833103e135a2e9dbf364
bappych <bappy4l@hotmai.com>
2023-03-10 16:01:24 +0000

    another for loop

--- For_loop.py
+++ For_loop.py
@@ -3,9 +3,13 @@ number_of_names = 23
 for i in range(0, number_of_names):
     print(i)
 
-# This is a new line added to check if the Git is working or not
 
-meals = ['pasta', 'pizza', 'salad']
+# A  different for loop
 
-for meal in meals:
+meals = input("Please enter the meals name")
+meals_name = [meals]
+
+for meal in meals_name:
     print(meal.capitalize())
+
+
